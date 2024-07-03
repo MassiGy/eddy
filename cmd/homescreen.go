32fe4fc43bcb416abbe5be108911c281e7066047
MassiGy <ghernaoutmassi@gmail.com>
2024-07-03 11:34:06 +0200

    fix: removed extra tabs to hide weird characters due to unsupported tabs on windows

--- cmd/homescreen.go
+++ cmd/homescreen.go
@@ -11,58 +11,58 @@ var homescreen_text string = `
                              |___/ 
 
 
-        Eddy is a vim like editor, meaning that we have multiple motions 
-        and modes. We currently support INSERT, NORMAL and PROMPT mode.
+Eddy is a vim like editor, meaning that we have multiple motions 
+and modes. We currently support INSERT, NORMAL and PROMPT mode.
 
-        INSERT is for updating the text buffer.
-        NORMAL is mainly for navigation and editing using motions.
-        PROMPT is for the command mode, it is like the ':' mode of vim.
+INSERT is for updating the text buffer.
+NORMAL is mainly for navigation and editing using motions.
+PROMPT is for the command mode, it is like the ':' mode of vim.
 
     
-        Some of eddy commands:  ( arrow keys are supported as well )
+Some of eddy commands:  ( arrow keys are supported as well )
 
-        Mode            Key         Name        Behavior
-        ---------------------------------------------------------------
-        NORMAL          i           insert      enter to INSERT mode.
-        NORMAL          e           edit        enter to INSERT mode.
-        NORMAL          ?           prompt      enter to PROMPT mode.
-        NORMAL          :           prompt      enter to PROMPT mode.
+Mode            Key         Name        Behavior
+---------------------------------------------------------------
+NORMAL          i           insert      enter to INSERT mode.
+NORMAL          e           edit        enter to INSERT mode.
+NORMAL          ?           prompt      enter to PROMPT mode.
+NORMAL          :           prompt      enter to PROMPT mode.
 
-        NORMAL          r           reload      reload the file. 
-        NORMAL          w           write       save the file. 
-        NORMAL          q           quit        quit the editor.
+NORMAL          r           reload      reload the file. 
+NORMAL          w           write       save the file. 
+NORMAL          q           quit        quit the editor.
 
-        NORMAL          h           vim.h       next character.
-        NORMAL          l           vim.l       prev character.
-        NORMAL          j           vim.j       goto next line.
-        NORMAL          h           vim.h       goto prev line.
-        NORMAL          f           vim.b       goto next word.
-        NORMAL          b           vim.b       goto prev word.
+NORMAL          h           vim.h       next character.
+NORMAL          l           vim.l       prev character.
+NORMAL          j           vim.j       goto next line.
+NORMAL          h           vim.h       goto prev line.
+NORMAL          f           vim.b       goto next word.
+NORMAL          b           vim.b       goto prev word.
 
-        NORMAL          d           vim.db      delete prev word.
-        NORMAL          D           vim.dw      delete next word.
-        NORMAL          c           vim.cb      change prev word.
-        NORMAL          C           vim.cw      change next word.
+NORMAL          d           vim.db      delete prev word.
+NORMAL          D           vim.dw      delete next word.
+NORMAL          c           vim.cb      change prev word.
+NORMAL          C           vim.cw      change next word.
 
-        NORMAL          I           vim.I       insert-in line start.
-        NORMAL          A           vim.A       insert-in line end.
-        NORMAL          o           vim.o       insert newline below.
-        NORMAL          O           vim.O       insert newline above.
-        NORMAL          y           vim.\"+yy    copy line to sysclip.
-        NORMAL          p           vim.\"+p     paste line from sysclip.
+NORMAL          I           vim.I       insert-in line start.
+NORMAL          A           vim.A       insert-in line end.
+NORMAL          o           vim.o       insert newline below.
+NORMAL          O           vim.O       insert newline above.
+NORMAL          y           vim.\"+yy    copy line to sysclip.
+NORMAL          p           vim.\"+p     paste line from sysclip.
 
-        NORMAL          u           vim.u       undo (infinite)
-        NORMAL          U           vim.CtrlR   redo (infinite)
+NORMAL          u           vim.u       undo (infinite)
+NORMAL          U           vim.CtrlR   redo (infinite)
 
-        ALL             Ctrl+s      write       save the file. 
-        ALL             Ctrl+r      reload      reload the file.
-        ALL             Ctrl+q      quit        quit the editor
-        ALL             Ctrl+n      vim.nu      toggle line numbers.
-        ALL             Esc         escape      goto NORMAL mode.
-        ALL             HOME        -           goto start of line.
-        ALL             END         -           goto end of line.
-        ALL             PgUp        page up     goto top of file.
-        ALL             PgDown      page down   goto end of file.
+ALL             Ctrl+s      write       save the file. 
+ALL             Ctrl+r      reload      reload the file.
+ALL             Ctrl+q      quit        quit the editor
+ALL             Ctrl+n      vim.nu      toggle line numbers.
+ALL             Esc         escape      goto NORMAL mode.
+ALL             HOME        -           goto start of line.
+ALL             END         -           goto end of line.
+ALL             PgUp        page up     goto top of file.
+ALL             PgDown      page down   goto end of file.
 `
 
 func get_homescreen_text() (buffer [][]rune) {
